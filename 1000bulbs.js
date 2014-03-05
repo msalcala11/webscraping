a948ab3100fee87f39157c7d06007ae0f2b43b8a
unknown <marty.alcala@GDS553.gdsassociates.com>
2014-03-05 14:40:49 -0500

    all previously separate files now combine into one big file

--- 1000bulbs.js
+++ 1000bulbs.js
@@ -4,6 +4,9 @@ var _ = require("underscore");
 var csv = require("csv");
 var async = require("async");
 
+//var setPAurl = 'http://www.homedepot.com/webapp/wcs/stores/servlet/StoreFinderViewDetails?storeZip=15235&langId=-1&recordId=4116&latitude=40.4307&storeCity=Pittsburgh&longitude=-79.80557&storeState=PA&catalogId=10053&storeFinderCartFlow=false&storeId=10051&ddkey=http:THDStoreFinderStoreSet'
+//var homedepotSearch = "http://www.homedepot.com/b/ENERGY-STAR-Certified/N-5yc1vZ1z0tlzw/Ntk-Extended/Ntt-9%2Bwatt%2Bcfl?rpp=96&Ns=P_REP_PRC_MODE%7C0&NCNI-5"
+
 var typeArr = [
 	{name: 'ENERGY STAR CFL Bulbs  - 9-11 W', start: 9, stop:11},
 	{name: 'ENERGY STAR CFL Bulbs  - 13-15 W', start: 13, stop: 15},
@@ -14,18 +17,18 @@ var typeArr = [
 var currentWattageArr = [];
 var page;
 currentWattage = 0;
-//var page = 'http://www.1000bulbs.com/search/?q='+currentWattage+'+watt+cfl&filter%5Battribute_energy_star%5D=Qualified&sort=price_a&page=2'
+var csvString = 'Category, Price, Wattage, Name, Source\n';
 var baseURL = "http://www.1000bulbs.com";
 var priceArr = [];
 var bulbArr = [];
 
 async.eachSeries(typeArr, function(typeItem, bigCallback){
 	currentWattageArr = generateWattageArr(typeItem.start, typeItem.stop);
-	console.log(currentWattageArr)
+
 	async.eachSeries(currentWattageArr, function(currentWattage, callback) {
 		console.log(currentWattage)
 		page = 'http://www.1000bulbs.com/search/?q='+currentWattage+'+watt+cfl&filter%5Battribute_energy_star%5D=Qualified&sort=price_a&page=1'
-	    scrapeCurrentWattage(currentWattage, callback);
+	    scrapeCurrentWattage(currentWattage, typeItem.name, callback);
 	
 	}, function(err) {
 
@@ -34,54 +37,38 @@ async.eachSeries(typeArr, function(typeItem, bigCallback){
 	});
 	
 }, function(err){
-	console.log('finished');
+  	csv()
+	.from(csvString)
+	.to('./1000bulbs.csv')
+	.on('end', function() {
+    	console.log('finished');
+  	})
 })
 
-
-//var currentWattage = 13;
-//var page = 'http://www.1000bulbs.com/search/?q=14+watt+cfl&filter[attribute_wattage]=10..19&filter[attribute_energy_star]=Qualified'
-//var page = 'http://www.1000bulbs.com/search/?q='+currentWattage+'+watt+cfl&filter%5Battribute_wattage%5D=10..19&filter%5Battribute_energy_star%5D=Qualified&sort=price_a&page=1'
-
-function scrapeCurrentWattage(currentWattage, callback) {
+function scrapeCurrentWattage(currentWattage, currentCategory, callback) {
 	console.log(page)
 	request({
 	  uri: page,
 	}, function(error, response, body) {
 	  	var $ = cheerio.load(body);
-	 	//console.log(body)
+
 	  	var catListElements = $(".CatList").nextAll();
 	  	count = catListElements.length
 	  	priceArr = [];
 		bulbArr = [];
 	  	$(".CatList").each(function() {
 
-		  	var elm = $(this);
-		  	var name = $(this).find(".first_list li")
-
-		  	// name.each(function(){
-		  	// 	lielm = $(this)
-		  	// 	if (lielm.text().search('13 Watt') != -1) {
-				 //  	//console.log("found one!")
-				 //  	//console.log($(this).text())
-			  // 		var priceStr = elm.find(".PriceIs").first().text()
-			  // 		var priceStrNoDollarSign = priceStr.replace("$", "");
-			  // 		var price = parseFloat(priceStrNoDollarSign);
-			  // 		//console.log(price)
-			  // 		priceArr.push(price)
-			  		
-		  	// 	}
-		  	// })
+	  		var elm = $(this);
 
 	  		// Let's grab the price of this bulb
 		  	var priceStr = elm.find(".PriceIs").first().text()
 			var priceStrNoDollarSign = priceStr.replace("$", "");
 			var price = parseFloat(priceStrNoDollarSign);
-			//console.log("price: " + price);
+
 			// Now Let's grab the product title
 			var title = $(this).find(".product-title-link")
 			var titleStr = title.text();
-			//console.log("title: " + titleStr)
-			//console.log("currentWattage: " + currentWattage)
+
 			// Only grab the low priced bulbs that have the specified wattage in the title (as a safety check)
 			if(price <= 7 && (titleStr.search(currentWattage + " Watt") != -1)){
 
@@ -90,8 +77,10 @@ function scrapeCurrentWattage(currentWattage, callback) {
 
 				// Let's create our bulb object with the data we just scraped for later insertion into csv file
 				var bulb = new Object();
-				bulb.title = titleStr;
+				bulb.category = currentCategory;
 				bulb.price = price;
+				bulb.wattage = currentWattage;
+				bulb.title = titleStr;
 				bulb.link = link;
 
 				bulbArr.push(bulb)
@@ -101,30 +90,23 @@ function scrapeCurrentWattage(currentWattage, callback) {
 
 			count--;
 		  	if(count === 3){
-
-		  		var csvString = 'Name, Price, Source\n';
+		  		
 		  		//Lets generate the string needed to generate the csv file
 		  		_.each(bulbArr, function(bulb){
-		  			newRowStr = bulb.title + "," + bulb.price + "," + bulb.link + "\n";
+		  			newRowStr = bulb.category + "," + bulb.price + "," + bulb.wattage + "," + bulb.title + "," + bulb.link + "\n";
 		  			csvString = csvString + newRowStr;
 		  		})
 
 		  		console.log("currentWattage: " + currentWattage)
 		  		console.log(priceArr)
 		  		console.log("avg price: " + average(priceArr))
-		  		//console.log(csvString)
 		  		
-		  		csv()
-				.from(csvString)
-				.to('./test'+currentWattage+'.csv')
-				//.end(function(){callback();})
 				callback();
 			}
 	  	});
 	});
 
 }
-
 
 function average(priceArr){
 	var sum = 0
